17f4688f7a38cd63f40e24b73a28a6270dcf575a
Kylie-T <kyliebellet@gmail.com>
2026-03-16 16:14:44 -0500

    "altered python; finished Main.java"

--- Main.java
+++ Main.java
@@ -1,28 +1,57 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         // function calls
-
-        // printing to the terminal
+        ArrayList<Integer> inputElements = inputValues();
+        minimum(inputElements);
+        average(inputElements);
     }
 
-    public char[] inputValues() {
+    public static ArrayList<Integer> inputValues() {
         // taking in user input
+        String elements = "[";
+        ArrayList<Integer> inputElements = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter single digit numbers separated by commas: ");
-        String userInput = scanner.nextline();
+        String userInput = scanner.nextLine();
+        scanner.close();
 
         // parsing through input
+        for (int i = 0; i < userInput.length(); i++) {
+            if (userInput.charAt(i) != ',') {
+                int number = Character.getNumericValue(userInput.charAt(i));
+                inputElements.add(number);
+                elements = elements + String.valueOf(number);
+                if (i < userInput.length() - 2){
+                    elements = elements + ", ";
+                }
+            }
+        }
+        System.out.println(elements + "]");
+        return inputElements;
     }
 
-    public int minimum() {
+    public static void minimum(ArrayList<Integer> inputElements) {
         // finding minimum number
-
+        int minElement = inputElements.get(0);
+        for (int i = 1; i < inputElements.size(); i++) {
+            if (inputElements.get(i) < minElement) {
+                minElement = inputElements.get(i);
+            }
+        }
+        System.out.println("Minimum number is: " + minElement);
     }
 
-    public float average() {
+    public static void average(ArrayList<Integer> inputElements) {
         // finding average number
-
+        float total = 0;
+        for (int i = 0; i < inputElements.size(); i++) {
+            total = total + inputElements.get(i);
+        }
+        float avgElement = (total / inputElements.size());
+        System.out.println("Average is: " + avgElement);
     }
 }
--- Main.py
+++ Main.py
@@ -4,36 +4,36 @@ def input_values():
     user_input = input("Enter single digit numbers separated by commas: ")
 
     # parsing through input
+    str_input_elements = []
     input_elements = []
+
     for value in user_input:
-        if value.isdigit():
-            input_elements.append(value)
+        if value.isdigit() and user_input.index(value) % 2 == 0:
+            str_input_elements.append(value)
+            input_elements.append(int(value))
+    print(f"Your numbers are: [{', '.join(str_input_elements)}]") 
     return input_elements
 
-def minimum():
+def minimum(input_elements):
     # finding minimum number
     min_element = input_elements[0]
     for i in range(1,len(input_elements)):
         if input_elements[i] < min_element:
             min_element = input_elements[i]
-    return min_element
+    print(f"Minimum number is: {min_element}")
 
-def average():
+def average(input_elements):
     # finding average number
     total = 0
     for value in input_elements:
         total += int(value)
     avg_num = total / len(input_elements)
-    return avg_num
+    print(f"Average is: {avg_num}\n")
 
 if __name__ == "__main__":
 
     # function calls
     input_elements = input_values()
-    min = minimum()
-    avg = average()
-
-    # printing to the terminal
-    print(f"Your numbers are: [{', '.join(input_elements)}]") 
-    print(f"Minimum number is: {min}")
-    print(f"Average is: {avg}\n")
+    min = minimum(input_elements)
+    avg = average(input_elements)
+    
